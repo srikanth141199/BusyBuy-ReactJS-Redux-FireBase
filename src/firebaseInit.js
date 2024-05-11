6421d0aa8dbac94fd10ea9cb309c962fea1ed0bd
Srikanth K <kolleparasrikanth@gmail.com>
2024-05-11 18:23:14 +0530

    Created basic React app and connected to fireBase

---
+++ src/firebaseInit.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDgjlCH1-669jwA9VEMN10lNHCkyC2H-10",
+  authDomain: "busybuy-srikanth.firebaseapp.com",
+  projectId: "busybuy-srikanth",
+  storageBucket: "busybuy-srikanth.appspot.com",
+  messagingSenderId: "46675944616",
+  appId: "1:46675944616:web:e358605c06c9bf33758e9d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
