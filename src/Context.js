dd73293759523e003d31ca4dfee8b6d072e21b9f
Srikanth K <kolleparasrikanth@gmail.com>
2024-05-12 13:56:29 +0530

    CartItem Implemented

--- src/Context.js
+++ src/Context.js
@@ -1,4 +1,5 @@
 import { createContext, useContext, useState } from "react";
+import { toast } from "react-toastify";
 
 const ItemContext = createContext();
 
@@ -22,6 +23,45 @@ function CustomItemContext({children}){
         setSignIn(false);
     }
 
+    const increaseQuantity = ({ id, price, name })=>{
+        const ind = cartItems.findIndex((item) => item.id === id)
+
+        if(ind === -1){
+            setCartItems([{price : price, name : name, quantity : 1}]);
+            
+        }
+        else{
+            const updatedCartItem = [...cartItems];
+            updatedCartItem[ind].quantity++;
+            setCartItems(updatedCartItem);
+        }
+        setTotalAmt(totalAmt + price)
+    }
+
+    const decreaseQuantity = ({id, price}) => {
+        const ind = cartItems.findIndex((item) => item.id === id);
+
+        if(ind !== -1){
+            const updatedCartItem = [...cartItems];
+            updatedCartItem[ind].quantity--;
+            setCartItems(updatedCartItem);
+            setTotalAmt(totalAmt - price)
+            if(updatedCartItem[ind].quantity === 0){
+                const id = updatedCartItem[ind].id
+                const price = updatedCartItem[ind].price
+                const name = updatedCartItem[ind].name 
+                handleRemoveToCart({id, price, name})
+            }
+        }
+    }
+
+    const handleRemoveToCart =({id, price, name})=>{
+        const updatedCartItems = cartItems.filter((cartItem) => cartItem.id !== id);
+        setCartItems(updatedCartItems);
+        setTotalAmt(totalAmt - price)
+        toast.success(`${name} remove to cart`);
+    }
+
     return(
         <ItemContext.Provider value = {{
             email, setEmail,
@@ -32,7 +72,10 @@ function CustomItemContext({children}){
             cartItems, setCartItems,
             totalAmt, setTotalAmt,
             isMyorder, setIsMyOrder,
-            myorder, setMyOrder
+            myorder, setMyOrder,
+            increaseQuantity,
+            decreaseQuantity,
+            handleRemoveToCart
         }}>
             {children}
         </ItemContext.Provider>
--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -1,5 +1,27 @@
-export default function CartItem(){
-    return(
-        <>CartItem</>
+import { useValue } from "../../Context"
+import "./CartItem.css"
+
+export default function CartItem({ name, id, price, img, quantity }) {
+
+    const {increaseQuantity, decreaseQuantity, handleRemoveToCart } = useValue();
+
+    
+    return (
+        <>
+            <div className="cartitem">
+                <img src={img} alt="" />
+                <div className="productdetails">
+                    <p>{name}</p>
+                    <div className="price">
+                        <h2>&#8377; {price}</h2>
+                        <img src="https://cdn-icons-png.flaticon.com/128/1828/1828899.png" alt="" onClick={() => decreaseQuantity({id, price})} />
+                        <div>{quantity}</div>
+                        <img src="https://cdn-icons-png.flaticon.com/128/1828/1828919.png" alt="" onClick={() => increaseQuantity({ id, price, name })} />
+                    </div>
+                </div>
+                <button onClick={() => handleRemoveToCart({id,price, name})}>Remove From Cart</button>
+            </div>
+
+        </>
     )
 }
