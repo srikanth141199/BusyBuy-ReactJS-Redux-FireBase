076697683fed76543da9d560344b658df305e811
Srikanth K <kolleparasrikanth@gmail.com>
2024-05-12 16:30:57 +0530

    url fix

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
       <CustomItemContext>
-        <BrowserRouter basename="/BusyBuySrikanth">
+        <BrowserRouter>
           <Navbar/>
           <Routes>
             <Route path="/" element = {<Home/>}/>
